b5d6faf0fcbee33fc3f6b26db50cee07ac61421d
jk <jkwak1635@gmail.com>
2024-10-08 01:24:28 +0900

    feat: implement left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,23 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main>Sidebar{children}</main>;
+  const loggedIn = { firstName: "JK", lastName: "Rowling" };
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
 }
