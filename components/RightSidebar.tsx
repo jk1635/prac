f3970d79ec15a6cee3ac4ed5b0650b39fdd79857
jk <jkwak1635@gmail.com>
2024-10-16 16:24:58 +0900

    feat: add getLoggedInUser

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import Link from "next/link";
 import Image from "next/image";
-import BankCard from "@/components/BankCard";
+import Link from "next/link";
+import React from "react";
+import BankCard from "./BankCard";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+const RightSidebar = async ({
+  user,
+  transactions,
+  banks,
+}: RightSidebarProps) => {
+  const loggedIn = await getLoggedInUser();
+  console.log("loggedIn", loggedIn);
+
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
@@ -23,6 +31,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
           </div>
         </div>
       </section>
+
       <section className="banks">
         <div className="flex w-full justify-between">
           <h2 className="header-2">My Banks</h2>
@@ -31,7 +40,8 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             <h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
           </Link>
         </div>
-        {banks.length > 0 && (
+
+        {banks?.length > 0 && (
           <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
             <div className="relative z-10">
               <BankCard
